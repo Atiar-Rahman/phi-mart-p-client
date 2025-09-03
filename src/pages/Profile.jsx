0609ec08ff76a70eaebd35349bafe99145c1a61c
Atiar-Rahman <atiar1709@gmail.com>
2025-09-03 17:48:43 +0600

    profile form edit or cancel

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ProfileForm from '../components/Dashboard/Profile/ProfileForm';
+import { useForm } from 'react-hook-form';
+import ProfileButton from '../components/Dashboard/Profile/ProfileButton';
 
 const Profile = () => {
+    const [isEditing,setIsEditing] = useState(false)
+    const {register,formState:{errors}} = useForm()
     return (
+        
         <div className='card w-full max-w-2xl mx-auto bg-base-100 shadow-xl'>
             <div className='card-body'>
                 <h2 className='card-title text-2xl'> my profile page</h2>
+                <form action="">
+                    <ProfileForm register={register} errors={errors} isEditing={isEditing} ></ProfileForm>
+                    <ProfileButton isEditing={isEditing} setIsEditing={setIsEditing}></ProfileButton>
+                </form>
             </div>
         </div>
     );
