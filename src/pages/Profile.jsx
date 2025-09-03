8e72f8528ec4833dc464209d56bd674790c3bbfc
Atiar-Rahman <atiar1709@gmail.com>
2025-09-03 19:47:48 +0600

    change password functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import ProfileForm from '../components/Dashboard/Profile/ProfileForm';
 import { useForm } from 'react-hook-form';
 import ProfileButton from '../components/Dashboard/Profile/ProfileButton';
+import PasswordChangeForm from '../components/Dashboard/Profile/PasswordChangeForm';
 
 const Profile = () => {
     const [isEditing,setIsEditing] = useState(false)
-    const {register,formState:{errors}} = useForm()
+    const {register,watch,formState:{errors}} = useForm()
     return (
         
         <div className='card w-full max-w-2xl mx-auto bg-base-100 shadow-xl'>
@@ -13,6 +14,7 @@ const Profile = () => {
                 <h2 className='card-title text-2xl'> my profile page</h2>
                 <form action="">
                     <ProfileForm register={register} errors={errors} isEditing={isEditing} ></ProfileForm>
+                    <PasswordChangeForm register={register} errors={errors} isEditing={isEditing} watch={watch}></PasswordChangeForm>
                     <ProfileButton isEditing={isEditing} setIsEditing={setIsEditing}></ProfileButton>
                 </form>
             </div>
