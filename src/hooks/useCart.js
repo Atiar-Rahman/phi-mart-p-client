00bd354d909752adc0b8ba7d3be944ad1db4dfdc
Atiar-Rahman <atiar1709@gmail.com>
2025-09-04 20:33:33 +0600

    update cart items quantity

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -6,35 +6,44 @@ import authApiClient from '../services/auth-api-client';
 const useCart = () => {
     const [authToken, setAuthToken] = useState(()=>JSON.parse(localStorage.getItem('authTokens')).access)
     const [cart,setCart] = useState(null)
-    const [cardId,setCardId] = useState(()=>localStorage.getItem('cartId'))
+    const [cartId,setCartId] = useState(()=>localStorage.getItem('cartId'))
     // create a new cart
     const createORGetCart = useCallback(async()=>{
         try{
             const response = await authApiClient.post('/carts/',{})
             // console.log(response.data)
-            if(!cardId){
+            if(!cartId){
                 localStorage.setItem('cartId',response.data.id)
-                setCardId(response.data.id)
+                setCartId(response.data.id)
             }
             setCart(response.data)
         }catch(err){
             console.log(err)
         }
-    },[cardId])
+    },[cartId])
 
     // add item to the cart
     const AddCartItems = useCallback(async(product_id,quantity)=>{
-        if(!cardId){
+        if(!cartId){
             await createORGetCart()
         }
         try{
-            const response = await authApiClient.post(`/carts/${cardId}/items/`,{product_id,quantity})
+            const response = await authApiClient.post(`/carts/${cartId}/items/`,{product_id,quantity})
             return response.data
         }catch(err){
             console.log("error adding item",err)
         }
-    },[cardId,createORGetCart])
-    return {cardId,cart,createORGetCart,AddCartItems}
+    },[cartId,createORGetCart])
+
+    // update item quantity
+    const updateCartItemQuantity = useCallback(async(itemId,quantity)=>{
+        try{
+            await authApiClient.patch(`/carts/${cartId}/items/${itemId}`,{quantity})
+        }catch(err){
+            console.log(err)
+        }
+    },[cartId])
+    return {cartId,cart,createORGetCart,AddCartItems,updateCartItemQuantity}
 };
 
 export default useCart;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,17 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import useCartContext from '../hooks/useCartContext';
+import CartItemList from '../components/Cart/CartItemList';
 
 const Cart = () => {
-    const { cart,createORGetCart } = useCartContext();
+    const { cart, createORGetCart, updateCartItemQuantity } = useCartContext();
 
     useEffect(()=>{
-        console.log("he")
+        // console.log("he")
         createORGetCart();
     },[createORGetCart])
+
+    const handleUpdateQuantity =async(itemId,newQuantity)=>{
+        try{
+            await updateCartItemQuantity(itemId,newQuantity)
+        }catch(err){
+            console.log(err)
+        }
+    }
+
     return (
+      <div className="flex justify-between">
         <div>
-            {JSON.stringify(cart)}
+          <Suspense fallback={<p>Loadding....</p>}>
+            <CartItemList
+              items={cart?.items || []} // ✅ match prop name
+              handleCartItemQuantity={handleUpdateQuantity} // ✅ match prop name
+            />
+          </Suspense>
         </div>
+        <div></div>
+      </div>
     );
 };
 
