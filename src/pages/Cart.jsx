2d497bd7b100cd646577c78f4a890562e85d1435
Atiar-Rahman <atiar1709@gmail.com>
2025-09-04 20:50:53 +0600

    update cart item both server and locally

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,16 +1,25 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import useCartContext from '../hooks/useCartContext';
 import CartItemList from '../components/Cart/CartItemList';
 
 const Cart = () => {
     const { cart, createORGetCart, updateCartItemQuantity } = useCartContext();
-
+    const [localCart,setLocalCart] = useState(cart);
     useEffect(()=>{
         // console.log("he")
         createORGetCart();
+
     },[createORGetCart])
 
+    useEffect(()=>{
+        setLocalCart(cart)
+    },[cart])
+
     const handleUpdateQuantity =async(itemId,newQuantity)=>{
+        // nested object update
+        setLocalCart((prevLocalCart)=>(
+            {...prevLocalCart,items:prevLocalCart.items.map(item=>item.id === itemId?{...item,quantity:newQuantity}:item)}
+        ))
         try{
             await updateCartItemQuantity(itemId,newQuantity)
         }catch(err){
@@ -23,8 +32,8 @@ const Cart = () => {
         <div>
           <Suspense fallback={<p>Loadding....</p>}>
             <CartItemList
-              items={cart?.items || []} // ✅ match prop name
-              handleCartItemQuantity={handleUpdateQuantity} // ✅ match prop name
+              items={localCart?.items || []} // ✅ match prop name
+              handleUpdateQuantity={handleUpdateQuantity} // ✅ match prop name
             />
           </Suspense>
         </div>
